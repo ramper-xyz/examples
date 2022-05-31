020adc81d6a4c347fa81c9c8d17ead5394ae54e6
Jay Hwang <octave08b@gmail.com>
2022-06-01 04:27:59 +0900

    feat: add CRA example

---
+++ create-react-app/src/App.js
@@ -0,0 +1,112 @@
+import { useState, useMemo } from "react";
+import {
+  init,
+  signIn,
+  getRamperSigner,
+  WALLET_PROVIDER,
+} from "@ramper/ethereum";
+import { ethers } from "ethers";
+import { get } from "lodash";
+
+init({
+  appName: "Ethereum Test App",
+  network: "maticmum",
+  authProviders: ["google", "facebook", "email", "apple", "twitter"],
+  walletProviders: ["metamask"],
+  theme: "dark",
+});
+
+const App = () => {
+  const [user, setUser] = useState();
+  const wallet = useMemo(
+    () => get(user, ["wallets", "ethereum"], null),
+    [user]
+  );
+  const [txResult, setTxResult] = useState();
+
+  const handleSignIn = async () => {
+    const signInResult = await signIn();
+    if (signInResult.error) return;
+
+    setUser(signInResult.user);
+  };
+
+  const handleSendFromWallet = async () => {
+    if (!wallet) {
+      return;
+    }
+
+    try {
+      if (wallet.provider === WALLET_PROVIDER.METAMASK) {
+        const ethereum = window.ethereum;
+        let provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+
+        const txResult = await signer.sendTransaction({
+          from: wallet.publicKey,
+          to: "0x4F08815944E166Bc76A8C91CE7dcD0f5BD5ae424",
+          value: "0.00001",
+          chainId: 80001,
+        });
+
+        console.log("txResult", txResult);
+        setTxResult(txResult);
+        return;
+      }
+
+      const alchemy = new ethers.providers.AlchemyProvider(
+        "maticmum",
+        "pEWvHrkSkkyWGZmezdGMk_LjYu8DAx1k"
+      );
+      const signer = await getRamperSigner(alchemy);
+
+      const fromAddress = wallet.publicKey;
+      const toAddress = "0x4F08815944E166Bc76A8C91CE7dcD0f5BD5ae424";
+      const value = "0.00001";
+
+      const gas = await alchemy.getFeeData();
+      const estimagedGas = await alchemy.estimateGas({
+        to: toAddress,
+        value: ethers.utils.parseEther(value),
+      });
+      const nonce = await alchemy.getTransactionCount(fromAddress);
+
+      const signedTx = await signer.signTransaction({
+        from: fromAddress,
+        to: toAddress,
+        value: ethers.utils.parseEther(value),
+        type: 2,
+        chainId: 80001,
+        nonce,
+        gasLimit: estimagedGas,
+        gasPrice: gas.maxFeePerGas,
+        maxFeePerGas: gas.maxFeePerGas,
+        maxPriorityFeePerGas: gas.maxPriorityFeePerGas,
+      });
+
+      const txResult = await alchemy.sendTransaction(signedTx);
+      setTxResult(txResult);
+      console.log(txResult);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <div>CRA Example</div>
+      <button onClick={handleSignIn}>Sign in</button>
+      <p>{JSON.stringify(user)}</p>
+      <button onClick={handleSendFromWallet}>Send from wallet</button>
+      <p>{JSON.stringify(txResult)}</p>
+    </div>
+  );
+};
+
+export default App;
